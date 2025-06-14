ec2a11ab0ff8409a84e670657084835136ea2557
balzhan abu <balzhanabu@MacBook-Air-balzhan.local>
2025-06-14 17:49:27 +0500

    Обновила main.py и поправила шаблоны

--- app/main.py
+++ app/main.py
@@ -1,28 +1,29 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
 
-from app.routers import clients, auth  # Роутеры
+from app.routers import clients, auth
 
 app = FastAPI(
     title="BlackDent — Учёт Пациентов",
     version="1.0.0"
 )
 
-# Подключение роутеров
+# Правильные пути
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+
+# Роутеры
 app.include_router(clients.router, prefix="/clients", tags=["Клиенты"])
 app.include_router(auth.router, tags=["Аутентификация"])
 
-# Корневой маршрут
 @app.get("/")
-def root():
-    return {"message": "Сервер работает!"}
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-# Подключение статики
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-# Favicon
 @app.get("/favicon.ico")
 async def favicon():
-    return FileResponse(os.path.join("app", "static", "favicon.ico"))
+    return FileResponse(os.path.join(BASE_DIR, "static", "favicon.ico"))
